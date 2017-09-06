a9f3f06c96bfc64763e9df116052b11ff60470f7
TakeakiOura <takeaki0811@gmail.com>
2017-09-06 00:07:10 +0000

    Signup-Login-Logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
       flash[:success] = "Welcome to the Sample App!"
       redirect_to root_path(@user)
     else
@@ -22,8 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,:password_confirmation)
   end
 
 end
